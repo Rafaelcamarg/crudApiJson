38ada84bb00a2074e1fb985dbf930123c408eb38
RafaelCamargo <139295033+Rafaelcamarg@users.noreply.github.com>
2023-07-21 20:07:25 -0300

    Add files via upload

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -10,6 +10,78 @@ async function listUsers(req: Request, res: Response) {
     res.send(JSON.parse(jsonData));
 }
 
+async function cUsers(req: Request, res: Response) {
+    //atribui a base de dados em nova variavel
+    const jsonDataBase = fs.readFileSync(data);
+    
+    //analisa string JSON e transforma em um objeto JavaScript
+    let content = JSON.parse(jsonDataBase);
+    
+    //verifica a quantidade de objetos na base de dados
+    let index: number = Object.keys(content).length;
+    
+    //criar uma nova chave de objeto somando +1 do total de objetos 
+    content[index++] = req.body;
+    
+    //analisa um objeto em JavaScript e transforma em uma string JSON
+    const values = JSON.stringify(content);
+    
+    //lê o arquivo da base de dados e adiciona o novo objeto
+    fs.writeFileSync(data, values);
+    
+    //retorno amigável para o usuário que chamou o endpoint
+    res.status(201).send("Users '" + req.body.username + "' registered with successfulll!");
+}
+
+async function eUsers(req: Request, res: Response) {
+    //atribui a base de dados em nova variavel
+    const jsonDataBase = fs.readFileSync(data);
+
+    //recupera o id enviado por parametro
+    const userId = req.params.id;
+    
+    //analisa string JSON e transforma em um objeto JavaScript
+    let content = JSON.parse(jsonDataBase);
+    
+    //atribui os dados da requisição ao usuario existente na base de dados
+    content[userId] = req.body;
+    
+    //analisa um objeto em JavaScript e transforma em uma string JSON
+    const values = JSON.stringify(content);
+    
+    //lê o arquivo da base de dados e adiciona o novo objeto
+    fs.writeFileSync(data, values);
+
+    //retorno amigável para o usuário que o endpoint
+    res.send(`User with id ${userId} has been updated`)
+}
+
+async function dUsers(req: Request, res: Response) {
+    //atribui a base de dados em nova variavel
+    const jsonDataBase = fs.readFileSync(data);
+
+    //recupera o id enviado por parametro
+    const userId = req.params.id;
+    
+    //analisa string JSON e transforma em um objeto JavaScript
+    let content = JSON.parse(jsonDataBase);
+    
+    //delete
+    delete content[userId];
+    
+    //analisa um objeto em JavaScript e transforma em uma string JSON
+    const values = JSON.stringify(content);
+
+    //lê o arquivo da base de dados e adiciona o novo objeto
+    fs.writeFileSync(data, values);
+
+    //retorno amigável para o usuário que o endpoint
+    res.send(`User with id ${userId} has been deleted`);
+}
+
 export default {
-    listUsers
+    listUsers,
+    cUsers,
+    eUsers,
+    dUsers
 }
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -11,83 +11,13 @@ const router = express.Router();
 
 //Listar registros
 router.get('/users', userController.listUsers);
-/*
-//Listar registros
-router.get('/users', (req: any, res: any) => {
-    const jsonData = fs.readFileSync(data);
-    //analisa string 
-    res.send(JSON.parse(jsonData));
-});
 
 //Cadastrar usuario
-router.post('/users', (req: any, res: any) => {
-    //atribui a base de dados em nova variavel
-    const jsonDataBase = fs.readFileSync(data);
-    
-    //analisa string JSON e transforma em um objeto JavaScript
-    let content = JSON.parse(jsonDataBase);
-    
-    //verifica a quantidade de objetos na base de dados
-    let index: number = Object.keys(content).length;
-    
-    //criar uma nova chave de objeto somando +1 do total de objetos 
-    content[index++] = req.body;
-    
-    //analisa um objeto em JavaScript e transforma em uma string JSON
-    const values = JSON.stringify(content);
-    
-    //lê o arquivo da base de dados e adiciona o novo objeto
-    fs.writeFileSync(data, values);
-    
-    //retorno amigável para o usuário que chamou o endpoint
-    res.status(201).send("User '" + req.body.username + "' registered with successfulll!");
-});
+router.post('/users', userController.cUsers);
 
 //Editar usuario
-router.put('/user/:id', (req: any, res: any) => {
-    //atribui a base de dados em nova variavel
-    const jsonDataBase = fs.readFileSync(data);
+router.put('/user/:id', userController.eUsers);
 
-    //recupera o id enviado por parametro
-    const userId = req.params.id;
-    
-    //analisa string JSON e transforma em um objeto JavaScript
-    let content = JSON.parse(jsonDataBase);
-    
-    //atribui os dados da requisição ao usuario existente na base de dados
-    content[userId] = req.body;
-    
-    //analisa um objeto em JavaScript e transforma em uma string JSON
-    const values = JSON.stringify(content);
-    
-    //lê o arquivo da base de dados e adiciona o novo objeto
-    fs.writeFileSync(data, values);
+router.delete('/user/:id', userController.dUsers);
 
-    //retorno amigável para o usuário que o endpoint
-    res.send(`User with id ${userId} has been updated`)
-});
-
-router.delete('/user/:id', (req: any, res: any) => {
-    //atribui a base de dados em nova variavel
-    const jsonDataBase = fs.readFileSync(data);
-
-    //recupera o id enviado por parametro
-    const userId = req.params.id;
-    
-    //analisa string JSON e transforma em um objeto JavaScript
-    let content = JSON.parse(jsonDataBase);
-    
-    //delete
-    delete content[userId];
-    
-    //analisa um objeto em JavaScript e transforma em uma string JSON
-    const values = JSON.stringify(content);
-
-    //lê o arquivo da base de dados e adiciona o novo objeto
-    fs.writeFileSync(data, values);
-
-    //retorno amigável para o usuário que o endpoint
-    res.send(`User with id ${userId} has been deleted`);
-});
-*/
 export default router;
